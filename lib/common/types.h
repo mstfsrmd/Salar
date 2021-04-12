d8ecd9f700ac50e1147f5034e4adb07ac0d88f72
Mostafa Sarmad <mostafasarmad96@gmail.com>
2021-04-12 21:20:32 +0430

    01222120

--- lib/common/types.h
+++ lib/common/types.h
@@ -1,74 +1,31 @@
 #ifndef _PRIMITIVE_BASIC_TYPES_H__
 #define _PRIMITIVE_BASIC_TYPES_H__
 
-typedef char                Char;
-typedef unsigned char       Byte;
-typedef enum {FALSE, TRUE}  Bool; 
-typedef void                Void;
+typedef int   Int32;
+typedef short Int16;
+typedef long long Int64;
 
-typedef char                Int8;
-typedef short               Int16;
-typedef int                 Int32;
-typedef long long           Int64;
-typedef unsigned char       UInt8;
-typedef unsigned short      UInt16;
-typedef unsigned int        UInt32;
-typedef unsigned long long  UInt64;
+typedef unsigned int   UInt32;
+typedef unsigned short UInt16;
+typedef unsigned long long UInt64;
 
-typedef unsigned long       UIntPtr; 
-typedef long                IntPtr;  
+typedef bool Bool;
+typedef void Void;
 
-typedef float               Float;
-typedef double              Double;
+typedef UInt32 Size;
+typedef Int32  SSize;
 
-//typedef unsigned long     Size;
-//typedef long              SSize;
-typedef unsigned int        Size;
-typedef int                 SSize;
-//typedef size_t            Size;
-//typedef ssize_t           SSize;
+typedef float  Float;
+typedef double Double;
 
-typedef Void*               Handle;
-typedef UInt32              Id;
+typedef char Char;
+typedef char Int8;
+typedef unsigned char Byte;
+typedef unsigned char Uint8;
 
-
-#define INVALID_HANDLE      NULL
-#define INVALID_ID          ((Id)-1)
-
-#define INFINITE_UINT32    ((UInt32)-1)
-#define INFINITE_UINT64    ((UInt64)-1)
-
-
-//---------------------------------
-
-
-#define GENERAL_VALUE_MAX_SIZE   8
-
-typedef Byte GeneralValueArray[GENERAL_VALUE_MAX_SIZE];
-
-
-typedef union {
-    Char              chr;
-    Byte              byt;
-    Bool              bl;
-    Int8              i8;
-    Int16             i16;
-    Int32             i32;
-    Int64             i64;
-    UInt8             u8;
-    UInt16            u16;
-    UInt32            u32;
-    UInt64            u64;
-    Float             flt;
-    Double            dbl;
-    Void*             ptr;
-    UIntPtr           uptr;
-    IntPtr            iptr;
-    Size              siz;
-    SSize             ssiz;
-    GeneralValueArray* general_value_array;
-}
-GeneralValue;
+typedef void*         Ptr;
+typedef long          IntPtr;
+typedef unsigned long UIntPtr;
 
 
 #endif  // _PRIMITIVE_BASIC_TYPES_H__
